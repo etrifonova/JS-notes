ed4d518707502a1eb90ac8233d248b29df3fd243
etrifonova <101653327+lenator@users.noreply.github.com>
2022-11-29 01:01:55 +0300

    Update html-academy js

--- html-academy/html-academy.js
+++ html-academy/html-academy.js
@@ -4,6 +4,31 @@
 
 */
 
+let materialPrice = {
+  'wood': 1000,
+  'stone': 1500,
+  'brick': 2000
+};
+
+let house = {
+  rooms: 10,
+  floors: 5,
+  material: 'wood',
+  coefficient: 10.5,
+
+  calculateSquare: function () {
+    return this.square = this.rooms * this.floors * this.coefficient;
+    },
+
+  calculatePrice: function () {
+    return this.price = materialPrice[this.material] * this.square;
+    }
+  };
+
+//  сработало!!!
+
+
+
 /* ОБЪЕКТЫ
 
 Объект — тип данных, который хранит в себе информацию в виде пар «ключ-значение».
@@ -52,6 +77,43 @@ cat.introduce(); // Выведет: Мяу! Я кот Рудольф!
 
 Важная деталь: пока функция не вызвана, this не содержит никакого значения, контекст появляется только в момент вызова функции.
 
+
+let processorPrice = {
+  'i5': 5000,
+  'i7': 10000
+};
+
+let displayPrice = {
+  13: 5000,
+  15: 10000
+};
+
+let memoryPrice = {
+  8: 3000,
+  16: 4000
+};
+
+let buildComputer = function (memory, display, processor) {
+  let customComputer = {
+    basicPrice: 5000,
+    processor: processor,
+    display: display,
+    memory: memory,
+
+    getDescription: function () {
+      return 'компьютер с процессором ' + this.processor + ', диагональю ' + this.display + ', оперативной памятью ' + this.memory;
+    },
+
+    getPrice: function () {
+      return this.basicPrice + processorPrice[this.processor] + displayPrice[this.display] + memoryPrice[this.memory];
+    }
+  };
+
+  return customComputer;
+};
+
+let myComputer = buildComputer(8, 13, 'i7');
+console.log('В корзине ' + myComputer.getDescription() + ' стоимостью ' + myComputer.getPrice());
 
 
 */
