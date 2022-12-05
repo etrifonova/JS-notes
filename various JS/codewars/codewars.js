5977e22e45a6e768293cd5b2546a816c4a65be6b
etrifonova <101653327+lenator@users.noreply.github.com>
2022-12-05 16:16:18 +0300

    Update codewars

--- various JS/codewars/codewars.js
+++ various JS/codewars/codewars.js
@@ -1,3 +1,51 @@
+/* Replace With Alphabet Position
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  return text;
+}
+
+*/
+
+
+
+
+// const alphaVal = (s) => s.toLowerCase().charCodeAt(0) - 97 + 1
+
+function alphabetPosition(text) {
+  let textArray = text.split('');
+  let numberArray = [];
+  for (let i = 0; i < textArray.length - 1; i++) {
+    numberArray += textArray[i].toLowerCase().charCodeAt(0) - 97 + 1 + " ";
+  }
+  numberArray += textArray[textArray.length - 1].toLowerCase().charCodeAt(0) - 97 + 1;
+  
+  return numberArray.toString();
+}
+
+console.log(alphabetPosition("hello world")) 
+// 8 5 12 12 15 -64 23 15 18 12 4 - ПРОБЕЛУ ПРИСВОЕН КОД -64, НУЖО ИГНОРИРОВАТЬ ПРОБЕЛЫ
+
+
+
+
+
+  // for (let i = 0; i < letterArray.length; i++) {
+  //   numberArray += letterArray[i].toLowerCase().charCodeAt(0) - 97 + 1;
+  // }
+
+// console.log(convertToNumber("baby"));
+
+
 /* Jaden Casing Strings
 Моё первое самостоятельное (без поиска задачи на Ютубе) решение:
 Решение громоздкое, но пока в рамках моих знаний. Надо учиться более рациональным решениям.
