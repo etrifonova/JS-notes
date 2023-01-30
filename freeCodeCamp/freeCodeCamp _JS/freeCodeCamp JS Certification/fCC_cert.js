284e61d343bf6d573a9b91658dc7979c6ec8481a
etrifonova <101653327+lenator@users.noreply.github.com>
2023-01-30 15:33:39 +0300

    feat: update js cert

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCamp JS Certification/fCC_cert.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCamp JS Certification/fCC_cert.js
@@ -1,3 +1,19 @@
+/* Caesars Cipher
+
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+
+
+*/
+
+
+
 /* Return true if the given string is a palindrome. Otherwise, return false.
 
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
@@ -110,3 +126,5 @@ function convertedNum (num) {
 console.log(convertedNum(3970));
 
 */
+
+
