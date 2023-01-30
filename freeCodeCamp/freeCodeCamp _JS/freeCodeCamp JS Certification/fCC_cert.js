9fd0fb5b7325b6a9251c52e3bb9853dadbdd80ca
etrifonova <101653327+lenator@users.noreply.github.com>
2023-01-30 12:22:50 +0300

    feat: update js cert

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCamp JS Certification/fCC_cert.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCamp JS Certification/fCC_cert.js
@@ -31,43 +31,104 @@ console.log(palindrome("1 eye for of 1 eye."));
 
 // Похожим образом работает метод Object.values, только на выходе получаем массив значений, а не ключей.
 
-const hogwartsStudents = [
-  { name: 'Harry',  house: 'Gryffindor' },
-  { name: 'Ron',  house: 'Gryffindor' },
-  { name: 'Hermione',  house: 'Gryffindor' },
-  { name: 'Draco',  house: 'Slytherin' },
-  { name: 'Voldemort',  house: 'Slytherin' },
-  { name: 'Luna',  house: 'Ravenclaw' },
-  { name: 'Cedric',  house: 'Hufflepuff' }
-]
+// const hogwartsStudents = [
+//   { name: 'Harry',  house: 'Gryffindor' },
+//   { name: 'Ron',  house: 'Gryffindor' },
+//   { name: 'Hermione',  house: 'Gryffindor' },
+//   { name: 'Draco',  house: 'Slytherin' },
+//   { name: 'Voldemort',  house: 'Slytherin' },
+//   { name: 'Luna',  house: 'Ravenclaw' },
+//   { name: 'Cedric',  house: 'Hufflepuff' }
+// ]
 
-const foundStudent = hogwartsStudents.find((student) => {
-  return student.house === 'Gryffindor';
-})
+// const foundStudent = hogwartsStudents.find((student) => {
+//   return student.house === 'Gryffindor';
+// })
 
-console.log(foundStudent)
+// console.log(foundStudent)
 
 
-const numConverter = [
-  { arabicNum: '1',  romanNum: 'I' },
-  { arabicNum: '2',  romanNum: 'II' },
-  { arabicNum: '3',  romanNum: 'III' },
-  { arabicNum: '4',  romanNum: 'IV' },
-  { arabicNum: '5',  romanNum: 'V' },
-  { arabicNum: '6',  romanNum: 'VII' },
-  { arabicNum: '7',  romanNum: 'VIII' }
-]
+
+
 
  
 //  console.log(convertToRoman(3));
 
-function convertedNum (number) {
-  return numConverter.find((num) => {
-    return num.arabicNum === number; 
+function convertedNum (num) {
+  const numConverter = [
+  { arabicNum: 1,  romanNum: 'I' },
+  { arabicNum: 2,  romanNum: 'II' },
+  { arabicNum: 3,  romanNum: 'III' },
+  { arabicNum: 4,  romanNum: 'IV' },
+  { arabicNum: 5,  romanNum: 'V' },
+  { arabicNum: 6,  romanNum: 'VI' },
+  { arabicNum: 7,  romanNum: 'VII' },
+  { arabicNum: 8,  romanNum: 'VIII' },
+  { arabicNum: 9,  romanNum: 'IX' },
+  { arabicNum10: 0,  romanNum: ' ' },
+  { arabicNum10: 1,  romanNum: 'X' },
+  { arabicNum10: 2,  romanNum: 'XX' },
+  { arabicNum10: 3,  romanNum: 'XXX' },
+  { arabicNum10: 4,  romanNum: 'XL' },
+  { arabicNum10: 5,  romanNum: 'L' },
+  { arabicNum10: 6,  romanNum: 'LX' },
+  { arabicNum10: 7,  romanNum: 'LXX' },
+  { arabicNum10: 8,  romanNum: 'LXXX' },
+  { arabicNum10: 9,  romanNum: 'XC' },
+  { arabicNum100: 0,  romanNum: ' ' },
+  { arabicNum100: 1,  romanNum: 'C' },
+  { arabicNum100: 2,  romanNum: 'CC' },
+  { arabicNum100: 3,  romanNum: 'CCC' },
+  { arabicNum100: 4,  romanNum: 'CD' },
+  { arabicNum100: 5,  romanNum: 'D' },
+  { arabicNum100: 6,  romanNum: 'DC' },
+  { arabicNum100: 7,  romanNum: 'DCC' },
+  { arabicNum100: 8,  romanNum: 'DCCC' },
+  { arabicNum100: 9,  romanNum: 'CM' },
+  { arabicNum1000: 1,  romanNum: 'M' }
+
+]
+  if (num.toString().length === 4) {
+    const numArray = num.toString().split('');
+    const newRoman = numConverter.find((number) => {
+      return number.arabicNum1000 == numArray[0]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum100 == numArray[0]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum10 == numArray[1]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum == numArray[2]; 
+    }).romanNum;
+    console.log(newRoman);
+    return newRoman;
+  } else if (num.toString().length === 3) {
+    const numArray = num.toString().split('');
+    const newRoman = numConverter.find((number) => {
+      return number.arabicNum100 == numArray[0]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum10 == numArray[1]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum == numArray[2]; 
+    }).romanNum;
+    console.log(newRoman);
+    return newRoman;
+  } else if (num.toString().length === 2) {
+    const numArray = num.toString().split('');
+    const newRoman = numConverter.find((number) => {
+      return number.arabicNum10 == numArray[0]; 
+    }).romanNum + numConverter.find((number) => {
+      return number.arabicNum == numArray[1]; 
+    }).romanNum;
+    console.log(newRoman);
+    return newRoman;
+  } else {
+    return numConverter.find((number) => {
+    return number.arabicNum === num; 
   }).romanNum;
+  }
 }
 
-console.log (convertedNum('4'));
+convertedNum(1003);
 
 
 // let array = Object.entries(numConverter).join(', ');
