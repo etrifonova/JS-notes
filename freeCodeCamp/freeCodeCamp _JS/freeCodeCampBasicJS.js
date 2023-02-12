d02d1e6093e769c67371da9bf1f00e3d7ec38bcc
etrifonova <101653327+etrifonova@users.noreply.github.com>
2023-02-13 00:48:33 +0300

    feat: update fcc js

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
@@ -42,6 +42,15 @@ const Window = function(tabs) {
   this.tabs = tabs; // We keep a record of the array inside the object
 };
 
+
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+
+console.log(this.tabs);
+// workWindow - объект с ключом tabs и значением в виде массива из 5 элементов
+
 // When you join two windows into one window
 Window.prototype.join = function(otherWindow) {
   this.tabs = this.tabs.concat(otherWindow.tabs);
@@ -49,40 +58,40 @@ Window.prototype.join = function(otherWindow) {
 };
 
 // When you open a new tab at the end
-Window.prototype.tabOpen = function(tab) {
-  this.tabs.push('new tab'); // Let's open a new tab for now
-  return this;
-};
+// Window.prototype.tabOpen = function(tab) {
+//   this.tabs.push('new tab'); // Let's open a new tab for now
+//   return this;
+// };
 
 // When you close a tab
-Window.prototype.tabClose = function(index) {
+// Window.prototype.tabClose = function(index) {
 
-  // Only change code below this line
+//   // Only change code below this line
 
-  const tabsBeforeIndex = this.tabs.splice(-1, index); // Get the tabs before the tab
-  const tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
+//   const tabsBeforeIndex = this.tabs.splice(-1, index); // Get the tabs before the tab
+//   const tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
 
-  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+//   this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
 
-  // Only change code above this line
+//   // Only change code above this line
 
-  return this;
- };
+//   return this;
+//  };
 
 // Let's create three browser windows
-const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
-const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
-const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
-console.log(workWindow);
-console.log(socialWindow);
-console.log(videoWindow);
+// const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+// const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+// const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+// console.log(workWindow);
+// console.log(socialWindow);
+// console.log(videoWindow);
 
 // Now perform the tab opening, closing, and other operations
-const finalTabs = socialWindow
-  .tabOpen() // Open a new tab for cat memes
-  .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
-  .join(workWindow.tabClose(1).tabOpen());
-console.log(finalTabs.tabs);
+// const finalTabs = socialWindow
+//   .tabOpen() // Open a new tab for cat memes
+//   .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+//   .join(workWindow.tabClose(1).tabOpen());
+// console.log(finalTabs.tabs);
 
 /* Understand Functional Programming Terminology
 The FCC Team had a mood swing and now wants two types of tea: green tea and black tea. General Fact: Client mood swings are pretty common.
