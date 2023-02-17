3858e48c0c2819147670aea53a143a9e7091458b
etrifonova <101653327+etrifonova@users.noreply.github.com>
2023-02-18 00:49:58 +0300

    feat: update fcc js

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
@@ -24,10 +24,33 @@
 
 */
 
-/*
+/* Implement the filter Method on a Prototype
+You might learn a lot about the filter method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You should not use the built-in filter method. The Array instance can be accessed in the myFilter method using this.
+
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+  for (let i=0; i<this.length; i++) {
+    if (callback(this[i], i, this) == true) {
+      newArray.push(this[i]);
+    }
+  }
+
+  // Only change code above this line
+  return newArray;
+};
+
+console.log([23, 65, 98, 5, 13].myFilter(item => item % 2));
+
+console.log(["naomi", "quincy", "camperbot"].myFilter(element => element === "naomi"));
+
+console.log([1, 1, 2, 5, 2].myFilter((element, index, array) => array.indexOf(element) === index));
+
+
 
 */
-
 /* Use the filter Method to Extract Data from an Array
 Another useful array function is Array.prototype.filter(), or simply filter().
 
