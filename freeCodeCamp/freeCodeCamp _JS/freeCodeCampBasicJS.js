9381d12e877a1600223d6ea92fa285af4f611d1f
etrifonova <101653327+etrifonova@users.noreply.github.com>
2023-03-03 10:22:21 +0300

    feat: update fcc js

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
@@ -17,9 +17,32 @@
 */
 
 
-/*
+/* DNA Pairing
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
 */
+
+function pairElement(str) {
+  let bigArray = [];
+  const arr = str.split('');
+  
+  for (let i = 0; i < arr.length - 1; i++) {
+    let smallArray =[];
+    smallArray.push(arr[i] + arr[i+1])
+    console.log(smallArray);
+    bigArray.push(smallArray);
+  }
+
+  return bigArray;
+}
+
+console.log(pairElement("ATCGA"));
 
 /* Search and Replace
 Perform a search and replace on the sentence using the arguments provided and return the new sentence.
