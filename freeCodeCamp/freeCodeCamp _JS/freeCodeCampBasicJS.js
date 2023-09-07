d231d8082da0d8870eb1b0e97c57692f7df0c607
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-09-07 15:28:04 +0300

    feat: add last task

--- freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
+++ freeCodeCamp/freeCodeCamp _JS/freeCodeCampBasicJS.js
@@ -5,8 +5,59 @@
 */
 
 /*
+Map the Debris
+According to Kepler's Third Law, the orbital period  T
+  of two point masses orbiting each other in a circular or elliptic orbit is:
+
+T=2πa3μ−−−√
+ 
+a
+  is the orbit's semi-major axis
+μ=GM
+  is the standard gravitational parameter
+G
+  is the gravitational constant,
+M
+  is the mass of the more massive body.
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
 
 */
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+ // Function to calculate orbital period (in seconds)
+ function calculateOrbitalPeriod(avgAltitude) {
+  // Convert altitude from kilometers to meters
+  const altitudeMeters = (earthRadius + avgAltitude) * 1000;
+
+  // Calculate semi-major axis (a) in meters
+  const semiMajorAxisMeters = altitudeMeters + earthRadius * 1000;
+
+  // Calculate orbital period using Kepler's Third Law
+  const orbitalPeriodSeconds = 2 * Math.PI * Math.sqrt(Math.pow(semiMajorAxisMeters, 3) / GM);
+
+  return Math.round(orbitalPeriodSeconds);
+}
+
+// Create a new array with orbital periods
+const newArray = arr.map(obj => ({
+  name: obj.name,
+  orbitalPeriod: calculateOrbitalPeriod(obj.avgAlt),
+}));
+
+return newArray;
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+
+
 
 /*
 Make a Person
