8783b32b3e9befc9c49721694ce7dc1f658c6c9d
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-09-29 14:28:48 +0300

    feat: style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
+// function Header() {
+//   return (
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" width="300"/>
+//       <p>
+//         About eTrifonova Learning Technologies
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Get a free lesson
+//       </a>
+//     </header>
+//   )
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
